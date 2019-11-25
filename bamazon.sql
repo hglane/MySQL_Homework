7712a2893d83138c32264fd0b92beeb5382ba880
Harris <hglane@crimson.ua.edu>
2019-11-24 21:10:57 -0600

    finished table with contents

--- bamazon.sql
+++ bamazon.sql
@@ -6,6 +6,38 @@ USE bamazon;
 
 CREATE TABLE products (
     item_id INTEGER NOT NULL AUTO_INCREMENT,
-    product_name VARCHAR(50) NOT NULL, 
-    
-)
+    product_name VARCHAR(50) NULL, 
+    department_name VARCHAR(50) NULL,
+    price DECIMAL (10,2) NULL,
+    stock_quanity INT NULL
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Ray-Ban Wayfarers", "Sunglasses", 109.99, 75)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Star Wars Boba Fett Playing Cards Card Game", "Games", 5.89, 3)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Blue Blocking Amber UV Protection Glasses", "Sunglasses", 11.95, 50)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("John Lennon Party Sunglasses", "Sunglasses", 8.69, 100)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("SENSO Bluetooth Headphones", "Audio", 29.99, 150)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Aromatherapy Essential Oil Diffuser", "Health & Household", 27.99, 225)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Aria Starr Dead Sea Mud Mask", "Personal Care", 14.95, 100)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Instant Pot", "Home & Kitchen", 59.86, 500)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Wool Dryer Balls", "Health & Household", 16.95, 100)
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Ott-Lite 24-Watt Floor Lamp", "Home & Kitchen", 65.28, 125)
