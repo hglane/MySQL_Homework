8aa3e2c42f6a83706ae5e7e151420ce946c2f373
Harris <hglane@crimson.ua.edu>
2019-11-24 22:00:38 -0600

    added to JS for search by ID

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -15,7 +16,25 @@ connection.connect(function(err) {
     if (err) throw err; 
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err; 
-        console.log(res)
-        connection.end();
+        console.log(res);
     });
 });
+
+function idSearch() {
+    inquirer 
+        .prompt({
+            name: "item_id",
+            type: "input",
+            message: "What is the ID of the product you would like to buy?"
+        })
+        .then(function(answer) {
+            var query = "SELECT product_name, department_name, price, stock_quantity FROM bamazon WHERE ?";
+            connection.query(query, { item_id: answer.item_id }, function(err, res) {
+                if (err) throw err;
+                for (var i = 0; i <res.length; i++) {
+                    console.log("Product Name: " + res[i].product_name + " || Department Name: " + res[i].department_name + " || Price: " + res[i].price + " || Stock Quantity: " + res[i].stock_quantity);
+                }
+                idSearch();
+            });
+        });
+}
