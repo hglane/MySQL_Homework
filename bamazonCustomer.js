6df399f6429422b53ba654a83431246a2fe2124c
Harris <hglane@crimson.ua.edu>
2020-02-08 21:33:37 -0600

    App successfully working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -36,7 +36,7 @@ function promptCustomerForItem(inventory) {
             {
                 type: "input",
                 name: "choice",
-                message: "What is the ID of the item you would like to purchase? [Quite with Q]",
+                message: "What is the ID of the item you would like to purchase? [Quit with Q]",
                 validate: function (val) {
                     return !isNaN(val) || val.toLowerCase() === "q";
                 }
@@ -57,11 +57,37 @@ function promptCustomerForItem(inventory) {
         });
 }
 
+function promptCustomerForQuantity(product) {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "quantity",
+                message: "How many would you like? [Quit with Q]",
+                validate: function (val) {
+                    return val > 0 || val.toLowerCase() === "q";
+                }
+            }
+        ])
+        .then(function(val) {
+            checkIfShouldExit(val.quantity);
+            var quantity = parseInt(val.quantity);
+
+            if (quantity > product.stock_quantity) {
+                console.log("\nInsufficient quantity!");
+                loadProducts();
+            }
+            else {
+                makePurchase(product, quantity);
+            }
+        });
+}
+
 function makePurchase(product, quantity) {
     connection.query(
         "UPDATE prodcuts SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
         [quantity, product.item_id],
-        function(err, res) {
+        function (err, res) {
             console.log("\nSuccesfully purchased " + quantity + " " + product.product_name + "'s!");
             loadProducts();
         }
@@ -69,7 +95,7 @@ function makePurchase(product, quantity) {
 }
 
 function checkInventory(choiceId, inventory) {
-    for (var i=0; i < inventory.length; i++) {
+    for (var i = 0; i < inventory.length; i++) {
         if (inventory[i].item_id === choiceId) {
             return inventory[i];
         }
